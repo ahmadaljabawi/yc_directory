c02d82e6a3fa7cf13ded0cf69b63f31175032a23
ahmadaljabawi <ahmedaljbaoe@gmail.com>
2024-12-10 16:59:03 +0300

    cant auth to git

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
+
 import { auth, signIn, signOut } from '@/auth'
 
 const Navbar = async () => {
     const session = await auth();
-
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
         <nav className='flex justify-between items-center'>
@@ -22,12 +22,10 @@ const Navbar = async () => {
                         <form action={async () => {
                             "use server";
 
-                            await signOut({ redirectTo: "/", redirect: true});
+                            await signOut({ redirectTo: "/"});
                         }}
                         >
-                            <button type="submit">
-                                <button type='submit'>Logout</button>
-                            </button>
+                            <button type='submit'>Logout</button>
                         </form>
 
                         <Link href={`/user/${session?.id}`}>
@@ -35,20 +33,18 @@ const Navbar = async () => {
                         </Link>
                     </>
                 ) : (
-                    <form action={async () => {
-                        "use server";
-
-                        await signIn('github'); //PROBLEM!! NOT REDIRECTING TO HOME PAGE
+                    <form
+                        action={async () => {
+                        "use server"
+                        await signIn('github');
                     }}>
-                        <button type='submit'>
-                            Login
-                        </button>
+                        <button type='submit'>Login</button>
                     </form>
                 )}
             </div>
         </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
